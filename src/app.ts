0b3ca549a164f8162df41d798358639034e3fd49
BART! <bart@bartweb.cz>
2022-01-20 12:33:23 +0100

    chore(app): functional approach

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 /**
- * @class GithubClickup
  *
  * Github extension adds a small button within each
  * PR detail to show information about ClickUp tasks...
@@ -12,27 +11,26 @@
 import { getClickupId, getPrItems } from './services/parser';
 import { createClickupLink } from './services/renderer';
 
-class GithubClickup {
-  constructor() {
-    const url = window.location.href.split('/');
-    const domain = url[2];
-    const page = url.pop();
+const githubClickup = () => {
+  const url = window.location.href.split('/');
+  const domain = url[2];
+  const page = url.pop();
 
-    if (domain.includes('github.') && page === 'pulls') {
-      const prItems = getPrItems();
+  if (domain.includes('github.') && page === 'pulls') {
+    const prItems = getPrItems();
 
-      for (const prItem of prItems) {
-        const title = prItem.querySelector('.markdown-title').textContent;
-        const clickupId = getClickupId(title);
+    for (const prItem of prItems) {
+      const title = prItem.querySelector('.markdown-title').textContent;
+      const clickupId = getClickupId(title);
 
-        if (title && clickupId) {
-          const elem = prItem.querySelector('a.markdown-title ~ div');
+      if (title && clickupId) {
+        const elem = prItem.querySelector('a.markdown-title ~ div');
 
-          createClickupLink(elem, clickupId);
-        }
+        createClickupLink(elem, clickupId);
       }
     }
   }
 }
 
-export default new GithubClickup();
+
+export default githubClickup();
